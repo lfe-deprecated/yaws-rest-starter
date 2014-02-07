011bf05d4b5cbe6989cf26110d7fd84945919d67
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-02-06 23:54:10 -0600

    Formatting.

--- package.exs
+++ package.exs
@@ -3,7 +3,7 @@ Expm.Package.new(
   description: "A Starter Project for RESTful Services in YAWS using LFE",
   version: "0.0.1",
   keywords: ["LFE", "Lisp", "Library", "Skeleton", "Starter",
-            "REST", "API", "HTTP", "Web"],
+             "REST", "API", "HTTP", "Web"],
   maintainers: [[name: "Duncan McGreggor",
                  email: "oubiwann@cogitat.io"]],
   repositories: [[github: "lfe/yaws-rest-starter"]])
