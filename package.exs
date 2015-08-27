02d104423db04925b68d6bf32c0509e20dd66e55
Duncan McGreggor <oubiwann@gmail.com>
2015-08-27 09:22:02 -0500

    Removed obselete EXPM.

--- package.exs
+++
@@ -1,9 +0,0 @@
-Expm.Package.new(
-  name: "yaws-rest-starter",
-  description: "A Starter Project for RESTful Services in YAWS using LFE",
-  version: "0.1.0",
-  keywords: ["LFE", "Lisp", "Library", "Skeleton", "Starter",
-             "REST", "API", "HTTP", "Web"],
-  maintainers: [[name: "Duncan McGreggor",
-                 email: "oubiwann@cogitat.io"]],
-  repositories: [[github: "lfex/yrests"]])
